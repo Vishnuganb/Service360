d1c3673625cc64769cd7938086ebadfe6aaf457d
Pranavan <spranavan008@gmail.com>
2023-07-19 16:28:30 +0530

    Mobile Responsive Fix #1

--- front-end/src/components/layout/ServiceProviderSidebar.js
+++ front-end/src/components/layout/ServiceProviderSidebar.js
@@ -10,21 +10,21 @@ import NavDropdown from 'react-bootstrap/NavDropdown';
 
 function ServiceProviderSideBar(){
     return(
-        <div className="container-fluid-lg ps-2">
+        <div className="container-fluid ps-2 m-0">
             <div className="row">
                 <div className="side-bar-sub-container col-auto col-md-2.5 min-vh-100 d-sm-inline d-flex justify-content-between flex-column">
                     <div className="sidebar-content">  
                         <ul className="nav nav-pills flex-column" id="parentM">
 
                             <li className="nav-item text-white fs-4 mt-4 py-2 py-sm-1">
-                                <a href="#" class="nav-link text-white">
+                                <a href="#" className="nav-link text-white">
                                     <i className="bi bi-speedometer2 " id="nav-single-element"></i>
                                     <span className="ms-3 d-none d-lg-inline ">Dashboard</span>
                                 </a>
                             </li>
 
                             <li className="nav-item text-white fs-4 my-0 py-2 py-sm-1">
-                                <a href="#submenu-1" class="nav-link text-white fs-5" data-bs-toggle="collapse">
+                                <a href="#submenu-1" className="nav-link text-white fs-5" data-bs-toggle="collapse">
                                     <i className="bi bi-house  d-none d-lg-inline d-sm-none d-md-none"></i>
                                     <span className="ms-3 d-none  d-none d-lg-inline d-sm-none d-md-none">My Services</span>
                                     <i className="bi bi-caret-down-fill ms-2 d-none d-lg-inline d-sm-none d-md-none"></i>
@@ -35,25 +35,25 @@ function ServiceProviderSideBar(){
                                         <NavDropdown.Item href="#serviceProvider" className="no-hover">My Vacancies</NavDropdown.Item>
                                     </NavDropdown>
                                 </a>
-                                <ul class="nav collapse ms-4.5 flex-column" id="submenu-1" data-bs-parent="#parentM">
-                                    <li class="nav-item fs-5 d-none d-lg-inline d-sm-none d-md-none">
-                                        <a class="nav-link text-white sub-item-nav" href="#">Jobs</a>
+                                <ul className="nav collapse ms-4.5 flex-column" id="submenu-1" data-bs-parent="#parentM">
+                                    <li className="nav-item fs-5 d-none d-lg-inline d-sm-none d-md-none">
+                                        <a className="nav-link text-white sub-item-nav ms-4" href="#">Jobs</a>
                                     </li>
-                                    <li class="nav-item fs-5 d-none d-lg-inline d-sm-none d-md-none">
-                                        <a class="nav-link text-white sub-item-nav" href="#">Vacancies</a>
+                                    <li className="nav-item fs-5 d-none d-lg-inline d-sm-none d-md-none">
+                                        <a className="nav-link text-white sub-item-nav ms-4" href="#">Vacancies</a>
                                     </li>
                                 </ul>
                             </li>
 
                             <li className="nav-item text-white fs-4 my-1 py-2 py-sm-1">
-                                <a href="#" class="nav-link text-white fs-5">
+                                <a href="#" className="nav-link text-white fs-5">
                                     <i className="bi bi-calendar-week" id="nav-single-element"></i>
                                     <span className="ms-3 d-none d-lg-inline">Availability Calendar</span>
                                 </a>
                             </li>
 
                             <li className="nav-item text-white fs-4 my-0 py-2 py-sm-1">
-                                <a href="#submenu-2" class="nav-link sub-sec text-white fs-5" data-bs-toggle="collapse" aria-current="page">
+                                <a href="#submenu-2" className="nav-link sub-sec text-white fs-5" data-bs-toggle="collapse" aria-current="page">
                                     <i className="bi bi-file-earmark-post  d-none d-lg-inline d-sm-none d-md-none"></i>
                                     <span className="ms-3 d-none d-lg-inline d-sm-none d-md-none">Blogs</span>
                                     <i className="bi bi-caret-down-fill ms-2 d-none d-lg-inline d-sm-none d-md-none"></i>
@@ -65,19 +65,19 @@ function ServiceProviderSideBar(){
                                     </NavDropdown>
 
                                 </a>
-                                <ul class="nav collapse ms-4.5 flex-column" id="submenu-2" data-bs-parent="#parentM">
-                                    <li class="nav-item fs-5 d-none d-lg-inline d-sm-none d-md-none">
-                                        <a class="nav-link text-white sub-item-nav" href="#">Create a Blog</a>
+                                <ul className="nav collapse ms-4.5 flex-column" id="submenu-2" data-bs-parent="#parentM">
+                                    <li className="nav-item fs-5 d-none d-lg-inline d-sm-none d-md-none">
+                                        <a className="nav-link text-white sub-item-nav ms-4" href="#">Create a Blog</a>
                                     </li>
-                                    <li class="nav-item fs-5 d-none d-lg-inline d-sm-none d-md-none">
-                                        <a class="nav-link text-white sub-item-nav" href="#">My Blogs</a>
+                                    <li className="nav-item fs-5 d-none d-lg-inline d-sm-none d-md-none">
+                                        <a className="nav-link text-white sub-item-nav ms-4" href="#">My Blogs</a>
                                     </li>
                                     
                                 </ul>
                             </li>
 
                             <li className="nav-item text-white fs-4 my-1 py-2 py-sm-1">
-                                <a href="#" class="nav-link text-white fs-5">
+                                <a href="#" className="nav-link text-white fs-5">
                                     <i className="bi bi-calendar2-plus " id="nav-single-element"></i>
                                     <span className="ms-3 d-none d-lg-inline">Create Sessions</span>
                                 </a>

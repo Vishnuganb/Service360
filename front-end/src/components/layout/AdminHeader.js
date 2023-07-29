400f98d24533e7dd01c016817171bb06be9aa54f
Balakrishnan Vishnugan <vishnuganb@gmail.com>
2023-07-29 14:48:18 +0530

    Admin Responsive Side Bar UI Completed

--- front-end/src/components/layout/AdminHeader.js
+++ front-end/src/components/layout/AdminHeader.js
@@ -17,9 +17,6 @@ function AdminHeader() {
                 <Navbar.Toggle aria-controls="basic-navbar-nav" />
                 <Navbar.Collapse id="basic-navbar-nav" className='justify-content-end'>
                     <Nav className="me-auto">
-                        <Nav.Link href="#jobs" className='fw-bold navLink'>Jobs</Nav.Link>
-                        <Nav.Link href="#vacancies" className='fw-bold navLink'>Vacancies</Nav.Link>
-                        <Nav.Link href="#blogs" className='fw-bold navLink'>Blogs</Nav.Link>
 
                         <Nav.Link href="#notifications" className='fw-bold navLink d-lg-inline d-sm-none d-md-none d-none'><i className="bi bi-bell-fill"></i></Nav.Link>
                         <Nav.Link href="#chat" className='fw-bold navLink d-lg-inline d-sm-none d-md-none d-none'><i className="bi bi-chat-fill"></i></Nav.Link>
@@ -27,10 +24,8 @@ function AdminHeader() {
                         <Nav.Link href="#notifications" className="fw-bold navLink d-sm-inline d-md-inline d-lg-none ">Notifications</Nav.Link>
                         <Nav.Link href="#chat" className="fw-bold navLink d-sm-inline d-md-inline d-lg-none ">Chat</Nav.Link>
 
-                        <NavDropdown title="Pranavan" className='fw-bold' id="basic-nav-dropdown">
+                        <NavDropdown title="Vishnugan" className='fw-bold' id="basic-nav-dropdown">
                             <NavDropdown.Item href="#" className="fw-bold no-hover">View Profile</NavDropdown.Item>
-                            <NavDropdown.Divider />
-                            <NavDropdown.Item href="#" className="fw-bold no-hover">FAQ</NavDropdown.Item>
                             <NavDropdown.Divider />
                             <NavDropdown.Item href="#" className="fw-bold no-hover">Logout</NavDropdown.Item>
                         </NavDropdown>
--- front-end/src/components/layout/AdminSidebar.js
+++ front-end/src/components/layout/AdminSidebar.js
@@ -1,197 +1,125 @@
-import * as React from 'react';
-import { styled, useTheme } from '@mui/material/styles';
-import Box from '@mui/material/Box';
-import MuiDrawer from '@mui/material/Drawer';
-import MuiAppBar from '@mui/material/AppBar';
-import Toolbar from '@mui/material/Toolbar';
-import List from '@mui/material/List';
-import CssBaseline from '@mui/material/CssBaseline';
-import Typography from '@mui/material/Typography';
-import Divider from '@mui/material/Divider';
-import IconButton from '@mui/material/IconButton';
-import MenuIcon from '@mui/icons-material/Menu';
-import ChevronLeftIcon from '@mui/icons-material/ChevronLeft';
-import ChevronRightIcon from '@mui/icons-material/ChevronRight';
-import ListItem from '@mui/material/ListItem';
-import ListItemButton from '@mui/material/ListItemButton';
-import ListItemIcon from '@mui/material/ListItemIcon';
-import ListItemText from '@mui/material/ListItemText';
-import InboxIcon from '@mui/icons-material/MoveToInbox';
-import MailIcon from '@mui/icons-material/Mail';
+import React,{useState} from "react";
+import 'bootstrap/dist/css/bootstrap.min.css'
+import 'bootstrap-icons/font/bootstrap-icons.css'
+import 'bootstrap/js/dist/dropdown'
+import 'bootstrap/js/dist/collapse'
+import '../../style/Admin/AdminSidebar.css'
+import NavDropdown from 'react-bootstrap/NavDropdown';
+import { Link } from 'react-router-dom';
+import Image from '../../assets/images/header/bg_1.png'
+import styles from "../../style/Admin/AdminSidebar.module.css";
 
-const drawerWidth = 240;
+function AdminSideBar() {
 
-const openedMixin = (theme) => ({
-    width: drawerWidth,
-    transition: theme.transitions.create('width', {
-        easing: theme.transitions.easing.sharp,
-        duration: theme.transitions.duration.enteringScreen,
-    }),
-    overflowX: 'hidden',
-});
-
-const closedMixin = (theme) => ({
-    transition: theme.transitions.create('width', {
-        easing: theme.transitions.easing.sharp,
-        duration: theme.transitions.duration.leavingScreen,
-    }),
-    overflowX: 'hidden',
-    width: `calc(${theme.spacing(7)} + 1px)`,
-    [theme.breakpoints.up('sm')]: {
-        width: `calc(${theme.spacing(8)} + 1px)`,
-    },
-});
-
-const DrawerHeader = styled('div')(({ theme }) => ({
-    display: 'flex',
-    alignItems: 'center',
-    justifyContent: 'flex-end',
-    padding: theme.spacing(0, 1),
-    // necessary for content to be below app bar
-    ...theme.mixins.toolbar,
-}));
-
-const AppBar = styled(MuiAppBar, {
-    shouldForwardProp: (prop) => prop !== 'open',
-})(({ theme, open }) => ({
-    zIndex: theme.zIndex.drawer + 1,
-    transition: theme.transitions.create(['width', 'margin'], {
-        easing: theme.transitions.easing.sharp,
-        duration: theme.transitions.duration.leavingScreen,
-    }),
-    ...(open && {
-        marginLeft: drawerWidth,
-        width: `calc(100% - ${drawerWidth}px)`,
-        transition: theme.transitions.create(['width', 'margin'], {
-            easing: theme.transitions.easing.sharp,
-            duration: theme.transitions.duration.enteringScreen,
-        }),
-    }),
-}));
-
-const Drawer = styled(MuiDrawer, { shouldForwardProp: (prop) => prop !== 'open' })(
-    ({ theme, open }) => ({
-        width: drawerWidth,
-        flexShrink: 0,
-        whiteSpace: 'nowrap',
-        boxSizing: 'border-box',
-        ...(open && {
-            ...openedMixin(theme),
-            '& .MuiDrawer-paper': openedMixin(theme),
-        }),
-        ...(!open && {
-            ...closedMixin(theme),
-            '& .MuiDrawer-paper': closedMixin(theme),
-        }),
-    }),
-);
-
-export default function AdminSidebar() {
-    const theme = useTheme();
-    const [open, setOpen] = React.useState(false);
-
-    const handleDrawerOpen = () => {
-        setOpen(true);
-    };
-
-    const handleDrawerClose = () => {
-        setOpen(false);
-    };
+    const [isOpen, setIsOpen] = useState(true);
+    const toggle = () => setIsOpen(!isOpen);
+    
 
     return (
-        <Box sx={{ display: 'flex' }}>
-            <CssBaseline />
-            <AppBar position="fixed" open={open}>
-                <Toolbar>
-                    <IconButton
-                        color="inherit"
-                        aria-label="open drawer"
-                        onClick={handleDrawerOpen}
-                        edge="start"
-                        sx={{
-                            marginRight: 5,
-                            ...(open && { display: 'none' }),
-                        }}
-                    >
-                        <MenuIcon />
-                    </IconButton>
-                    <Typography variant="h6" noWrap component="div">
-                        Mini variant drawer
-                    </Typography>
-                </Toolbar>
-            </AppBar>
-            <Drawer variant="permanent" open={open}>
-                <DrawerHeader>
-                    <IconButton onClick={handleDrawerClose}>
-                        {theme.direction === 'rtl' ? <ChevronRightIcon /> : <ChevronLeftIcon />}
-                    </IconButton>
-                </DrawerHeader>
-                <Divider />
-                <List>
-                    {['Inbox', 'Starred', 'Send email', 'Drafts'].map((text, index) => (
-                        <ListItem key={text} disablePadding sx={{ display: 'block' }}>
-                            <ListItemButton
-                                sx={{
-                                    minHeight: 48,
-                                    justifyContent: open ? 'initial' : 'center',
-                                    px: 2.5,
-                                }}
-                            >
-                                <ListItemIcon
-                                    sx={{
-                                        minWidth: 0,
-                                        mr: open ? 3 : 'auto',
-                                        justifyContent: 'center',
-                                    }}
+        <div className={`${styles.containerFluid4} ps-2 m-0 ${isOpen ? styles.largeScreen : styles.smallScreen}`}>
+            <div className="row h-100">
+                <div className="side-bar-sub-container col-auto col-md-2.5 min-vh-100 d-xs-inline d-flex justify-content-between flex-column" style={{ backgroundImage: `url(${Image})`}}>
+                    <div className="sidebar-content">
+                        <ul className="nav nav-pills flex-column" id="parentM">
+
+                            <li className={`fs-4 mt-3 py-2 d-flex d-none d-lg-inline ${isOpen ? "align-self-end" : "mx-3"}`} onClick={toggle}
+                                style={{
+                                    position: 'relative',
+                                    cursor: 'pointer',
+                                }}>
+                                <i className="bi bi-list text-white" style={{ marginLeft: 'auto' }}></i>
+                            </li>
+
+                            <li className="nav-item text-white fs-4 mt-4 py-2 py-xs-1">
+                                <Link to="/admin/dashboard" className="nav-link nav-link-sidebar text-white">
+                                    <i className="bi bi-grid"></i>
+                                    <span className={`ms-3 d-none ${isOpen ? "d-lg-inline" : "d-none"}`}>Dashboard</span>
+                                </Link>
+                            </li>
+
+                            <li className="nav-item text-white fs-4 my-0 py-2 py-xs-1">
+                                <Link to="/admin/services" className="nav-link nav-link-sidebar text-white">
+                                    <i className="bi bi-tools" id="nav-single-element" style={{ fill: "none" }}></i>
+                                    <span className={`ms-3 d-none ${isOpen ? "d-lg-inline" : "d-none"}`}>Services</span>
+                                </Link>
+                            </li>
+
+                            <li className="nav-item text-white fs-4 my-0 py-2 py-xs-1">
+                                <Link to="#submenu-1"
+                                    className={`nav-link nav-link-sidebar text-white d-none ${isOpen ? "d-lg-inline" : "d-none"}`}
+                                    data-bs-toggle="collapse"
+                                    aria-current="page"
                                 >
-                                    {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
-                                </ListItemIcon>
-                                <ListItemText primary={text} sx={{ opacity: open ? 1 : 0 }} />
-                            </ListItemButton>
-                        </ListItem>
-                    ))}
-                </List>
-                <Divider />
-                <List>
-                    {['All mail', 'Trash', 'Spam'].map((text, index) => (
-                        <ListItem key={text} disablePadding sx={{ display: 'block' }}>
-                            <ListItemButton
-                                sx={{
-                                    minHeight: 48,
-                                    justifyContent: open ? 'initial' : 'center',
-                                    px: 2.5,
-                                }}
-                            >
-                                <ListItemIcon
-                                    sx={{
-                                        minWidth: 0,
-                                        mr: open ? 3 : 'auto',
-                                        justifyContent: 'center',
-                                    }}
+                                    <i className={`bi bi-people d-none ${isOpen ? "d-lg-inline" : "d-none"}`}></i>
+                                    <span className={`ms-3 d-none ${isOpen ? "d-lg-inline" : "d-none"}`}>Users</span>
+                                    <i className={`bi bi-chevron-down ms-2 d-none ${isOpen ? "d-lg-inline" : "d-none"}`}></i>
+                                </Link>
+
+                                <NavDropdown
+                                    title={<> <i className="bi bi-people ps-3"></i> </> }
+                                    id="basic-nav-dropdown"
+                                    className={`navlink dropdown-icon d-md-inline d-xs-inline custom-dropdown ${isOpen ? " d-lg-none" : "d-lg-inline"}`}
                                 >
-                                    {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
-                                </ListItemIcon>
-                                <ListItemText primary={text} sx={{ opacity: open ? 1 : 0 }} />
-                            </ListItemButton>
-                        </ListItem>
-                    ))}
-                </List>
-            </Drawer>
-            <Box component="main" sx={{ flexGrow: 1, p: 3 }}>
-                <DrawerHeader />
-                <Typography paragraph>
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-                    tempor incididunt ut labore et dolore magna aliqua. Rhoncus dolor purus non
-                    enim praesent elementum facilisis leo vel. Risus at ultrices mi tempus
-                    
-                </Typography>
-                <Typography paragraph>
-                    Consequat mauris nunc congue nisi vitae suscipit. Fringilla est ullamcorper
-                    eget nulla facilisi etiam dignissim diam. Pulvinar elementum integer enim
-                    neque volutpat ac tincidunt. Ornare suspendisse sed nisi lacus sed viverra
-                    tellus. Purus sit amet volutpat consequat mauris.
-                </Typography>
-            </Box>
-        </Box>
+                                    <NavDropdown.Item as={Link} to="/admin/customers" className="no-hover"> Customers </NavDropdown.Item>
+                                    <NavDropdown.Divider />
+                                    <NavDropdown.Item as={Link} to="/admin/serviceProviders" className="no-hover"> Service Providers </NavDropdown.Item>
+                                    <NavDropdown.Divider />
+                                    <NavDropdown.Item as={Link} to="/admin/advertisers" className="no-hover"> Advertisers </NavDropdown.Item>
+                                </NavDropdown>
+                                
+                                <ul
+                                    className="nav collapse ms-4.5 flex-column"
+                                    id="submenu-1"
+                                    data-bs-parent="#parentM"
+                                >
+                                    <li className={`nav-item fs-5 ${isOpen ? "d-lg-inline" : "d-none"}`}>
+                                        <Link to="/admin/customers" className="nav-link nav-link-sidebar text-white sub-item-nav ms-4" > Customers </Link>
+                                        <Link to="/admin/serviceProviders" className="nav-link nav-link-sidebar text-white sub-item-nav ms-4"> Service Providers </Link>
+                                        <Link to="/admin/advertisers" className="nav-link nav-link-sidebar text-white sub-item-nav ms-4"> Advertisers </Link>
+                                    </li>
+                                </ul>
+                            </li>
+
+                            <li className="nav-item text-white fs-4 my-1 py-2 py-xs-1">
+                                <Link to="/admin/advertisements" className="nav-link nav-link-sidebar text-white">
+                                    <i className="bi bi-window-stack"></i>
+                                    <span className={`ms-3 d-none ${isOpen ? "d-lg-inline" : "d-none"}`}> Advertisements </span> 
+                                </Link>
+                            </li>
+
+                            <li className="nav-item text-white fs-4 my-0 py-2 py-xs-1">
+                                <Link to="/admin/review" className="nav-link nav-link-sidebar text-white" >
+                                    <i className="fas fa-star-half-alt"></i>
+                                    <span className={`ms-3 d-none ${isOpen ? "d-lg-inline" : "d-none"}`}> Reviews </span>
+                                </Link>
+                            </li>
+
+                            <li className="nav-item text-white fs-4 my-0 py-2 py-xs-1">
+                                <Link to="/admin/invoice" className="nav-link nav-link-sidebar text-white" >
+                                    <i className="bi bi-receipt"></i>
+                                    <span className={`ms-3 d-none ${isOpen ? "d-lg-inline" : "d-none"}`}> Invoice</span>
+                                </Link>
+                            </li>
+
+                            <li className="nav-item text-white fs-4 my-0 py-2 py-xs-1">
+                                <Link to="/admin/complaints" className="nav-link nav-link-sidebar text-white" >
+                                    <i className="bi bi-person-exclamation"></i>
+                                    <span className={`ms-3 d-none ${isOpen ? "d-lg-inline" : "d-none"}`}> Complaints</span>
+                                </Link>
+                            </li>
+
+                            <li className="nav-item text-white fs-4 my-1 py-2 py-xs-1">
+                                <Link to="/admin/report" className="nav-link nav-link-sidebar text-white" >
+                                    <i className="bi bi-clipboard-data" ></i>
+                                    <span className={`ms-3 d-none ${isOpen ? "d-lg-inline" : "d-none"}`}> Reports </span>
+                                </Link>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
     );
 }
+
+export default AdminSideBar;

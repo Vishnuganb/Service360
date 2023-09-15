979134d9b4423a1d37eb4604173ecddbce115299
Balakrishnan Vishnugan <vishnuganb@gmail.com>
2023-09-15 16:50:59 +0530

    Customers Details Verification Completed and Service Provider Registration Error Fixed

--- back-end/src/main/java/com/service360/group50/repo/UserRepository.java
+++ back-end/src/main/java/com/service360/group50/repo/UserRepository.java
@@ -1,10 +1,12 @@
 package com.service360.group50.repo;
 
+import com.service360.group50.entity.Role;
 import com.service360.group50.entity.Users;
 import jakarta.transaction.Transactional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.time.LocalDate;
@@ -13,11 +15,8 @@ import java.util.Optional;
 
 @Repository
 public interface UserRepository extends JpaRepository<Users, Long> {
-    Optional<Users> findByEmail ( String email );
 
     List<Users> findAllByRegistrationdateBetween ( LocalDate startDate, LocalDate endDate );
-
-    List<Users> findByRole ( String role );
 
     Optional<Users> findByEmailIgnoreCase(String email);
 
@@ -26,5 +25,7 @@ public interface UserRepository extends JpaRepository<Users, Long> {
     @Query("UPDATE Users u " +
             "SET u.enabled = TRUE WHERE u.email = ?1")
     int enableUser(String email);
+
+    List<Users> findByRoleAndEnabled( Role role, boolean enabled);
 
 }
